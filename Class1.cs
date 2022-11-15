745d4273468c005bb2c789898099ea8038623d11
t1nagree <isip_r.r.ismailov@mpt.ru>
2022-11-15 17:54:14 +0400

    ПЫТАЮСЬ В ТРЕТИЙ РАЗ ЛАБУ ЗАГРУЗИТЬЬЬЬgit add .git add .

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿Random ugaday = new Random();
+int i = ugaday.Next(100);
+i = Convert.ToInt32(i);
+int attemp = 0;
+
+int k;
+Console.WriteLine("Сделайте попытку и угадайте число от 0 до 100, загаданное алгоритмом компьютера");
+do
+{
+    Console.WriteLine("а")
+
+
+}    
